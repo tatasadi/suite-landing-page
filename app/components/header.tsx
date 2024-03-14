87ac79649f664386a0ba19be7825c4ed07588074
Ehsan Tatasadi <ehsan@tatasadi.com>
2024-03-14 16:05:41 +0100

    add heading styles

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,5 +1,12 @@
+import logo from "@/public/logo.svg"
+import Image from "next/image"
+
 const Header = () => {
-  return <header className=""></header>
+  return (
+    <header className="">
+      <Image src={logo} alt="logo" />
+    </header>
+  )
 }
 
 export default Header
